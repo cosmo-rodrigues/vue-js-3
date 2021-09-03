35143444b4580f36dd01a389b53e59faf82920bd
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-09-03 02:53:06 -0300

    First commit

---
+++ currency-app/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container grid-lg my-2 py-2">
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+      <div class="card-body">
+        <ListQuotes :quotes="quotes"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { onMounted, reactive, toRefs } from 'vue'
+  import api from '@/services/api'
+  import ListQuotes from './components/ListQuotes'
+
+  export default {
+    name: 'App',
+    components: {
+      ListQuotes,
+    },
+    setup() {
+      const data = reactive({
+        quotes: {},
+      })
+      onMounted(async () => {
+        const response = await api.all()
+        data.quotes = response.data
+      })
+      return { ...toRefs(data) }
+    }
+  }
+</script>
---
+++ currency-app/src/components/ListQuotes.vue
@@ -0,0 +1,42 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Mínimo</th>
+        <th>Máximo</th>
+        <th>Variação</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(quote, key) in quotes" :key="key">
+        <td>{{ key }}</td>
+        <td>{{ quote.name }}</td>
+        <td>{{ quote.high }}</td>
+        <td>{{ quote.low }}</td>
+        <td>
+          <span
+            :class="[
+              'label', 'label-rounded', 'text-small',
+              {'label-error': quote.pctChange < 0, 'label-success': quote.pctChange > 0}
+            ]"
+          >
+            {{ quote.pctChange }} %
+          </span>
+        </td>
+        <th></th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  props: {
+    quotes: {
+      type: Object, required: true,
+    },
+  }
+}
+</script>
